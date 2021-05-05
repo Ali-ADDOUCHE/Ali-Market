24ffb81c5de62cbbcd58a39da4ce5d7d61a73849
Add Lie <46305416+Ali-ADDOUCHE@users.noreply.github.com>
2021-05-05 18:54:27 +0300

    Add files via upload

---
+++ AliMarket/MyStore/Properties/AssemblyInfo.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+
+[assembly: AssemblyTitle("MyStore")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MyStore")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7d354f78-6ad6-4734-9150-ce033c7ef6fa")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
